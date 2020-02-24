f2579166cc448db24a5697481028717cad561485
dernathul <lautaroparracab@gmail.com>
2020-02-24 13:54:38 +0100

    adding the first test

--- bmi_calculator/src/App.jsx
+++ bmi_calculator/src/App.jsx
@@ -1,10 +1,24 @@
 import React, { Component } from "react";
+import Form from "./components/Form";
 
 class App extends Component {
+  state = {
+    weight: "",
+    height: ""
+  };
+
+  onChangeHandler = e => this.setState({ [e.target.name]: e.target.value });
+
   render() {
-    return <div style={{fontSize: 40, color: "purple" }}>
-      Hello World and Mr.Lau CEO of Git, and co-founder of yarn
-    </div>;
+    return (
+      <div>
+        <Form
+          weight={this.state.weight}
+          height={this.state.height}
+          onChangeHandler={this.onChangeHandler}
+        />
+      </div>
+    );
   }
 }
 
