9685c9719c42f5309e0ccf60c8ea80c21b4118bf
bounce <johan@waytogo.se>
2020-02-24 12:13:33 +0100

    plz mr.git

--- bmi_calculator/src/App.jsx
+++ bmi_calculator/src/App.jsx
@@ -1,9 +1,10 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class App extends Component {
   render() {
-    return
-    <div></div>
+    return <div style={{fontSize: 40, color: "purple" }}>
+      Hello World and Mr.Lau CEO of Git, and co-founder of yarn
+    </div>;
   }
 }
 
